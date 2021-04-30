4aa99d47119c404d4ba6dfb3d9474e9adb3767b4
Edith <edithpuclla20@gmail.com>
2021-04-30 16:55:32 -0500

    lifoff

---
+++ 02-week/05-lesson-expressions/khansole/liftoff.py
@@ -0,0 +1,13 @@
+"""
+Prints out a spaceship launch sequence.
+"""
+
+MAX_COUNT = 10
+
+def main():
+    for i in range(MAX_COUNT, 0, -1):
+        print(i)
+    print("Liftoff!")
+
+if __name__ == '__main__':
+    main()
