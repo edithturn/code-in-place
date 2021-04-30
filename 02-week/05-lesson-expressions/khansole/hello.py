aecd45868b5b7e8460cbfe7c8c6656cc1194c8d4
Edith <edithpuclla20@gmail.com>
2021-04-30 00:28:49 -0500

    hello customize

---
+++ 02-week/05-lesson-expressions/khansole/hello.py
@@ -0,0 +1,12 @@
+"""
+Prompts the user for their name and then says hello!
+"""
+
+def main():
+    # Ask for the name of the user
+    name = input("What is your name? ")
+    # Pritn hello name
+    print(f"Hello " + name + "!")
+
+if __name__ == '__main__':
+    main()
